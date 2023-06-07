4ad7d7b0d7d6eab9d9996ecb4c2f2c8396d6c42f
Nhan NguyenM <nhan.nguyenm@s3corp.com.vn>
2023-06-07 16:08:32 +0700

    install reactjs and tailwindcss

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-2xl bg-sky-300 font-bold text-left text-red-500'>Hello World</h1>
+
+    </div>
+  );
+}
+
+export default App;
