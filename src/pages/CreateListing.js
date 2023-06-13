92fa0f1ab174041cbd6432278036a635e095b144
Nhan NguyenM <nhan.nguyenm@s3corp.com.vn>
2023-06-13 16:56:48 +0700

    complete create listing page

--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Spinner from '../components/Spinner';
+import { toast } from 'react-toastify';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { getAuth } from 'firebase/auth';
+import { v4 as uuidv4 } from "uuid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
+import { useNavigate } from 'react-router';
 
 export default function CreateListing() {
 
+    const navigate = useNavigate();
+    const auth = getAuth();
+    const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+    const [loading, setLoading] = useState(false)
     const [formData, setFormData] = useState({
         type: "rent",
         name: "",
@@ -14,18 +26,163 @@ export default function CreateListing() {
         offer: false,
         regularPrice: 0,
         discountedPrice: 0,
+        images: {},
+        latitude: 0,
+        longitude: 0
     });
 
-    const { type, name, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountedPrice } = formData;
+    const {
+        type,
+        name,
+        bedrooms,
+        bathrooms,
+        parking,
+        furnished,
+        address,
+        description,
+        offer,
+        regularPrice,
+        discountedPrice,
+        images,
+        latitude,
+        longitude
+    } = formData;
 
-    function onChange() {
+    function onChange(e) {
+        let boolean = null;
+        if (e.target.value === "true") {
+            boolean = true;
+        }
+        if (e.target.value === "false") {
+            boolean = false;
+        }
 
+        // Files
+        if (e.target.files) {
+            setFormData((formData) => ({
+                ...formData,
+                // khi người dùng chọn vào input có type = file giá trị của e.target.files sẽ được gán vào images trong formData
+                images: e.target.files
+            }));
+        }
+
+        // Text, Boolean, Number
+        if (!e.target.files) {
+            setFormData((formData) => ({
+                ...formData,
+                [e.target.id]: boolean ?? e.target.value,
+            }));
+        }
+    }
+
+    async function onSubmit(e) {
+        e.preventDefault();
+        setLoading(true);
+        
+        // Kiểm tra tính hợp lễ đầu vào
+        if (+discountedPrice >= +regularPrice) {
+            setLoading(false)
+            toast.error("Discounted price needs to be less than regular price")
+            return;
+        }
+        if (images.length > 6) {
+            setLoading(false)
+            toast.error("Maximum 6 images are allowed");
+            return;
+        }
+
+        // Kiểm tra vị trí của người dùng nhập vào
+        let geolocation = {};
+        let location;
+        if (geolocationEnabled) {
+            const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`);
+            const data = await response.json();
+            geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+            geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+
+            location = data.status === "ZERO_RESULTS" && undefined;
+
+            if (location === undefined) {
+                setLoading(false);
+                toast.error("Please enter a correct address")
+                return;
+            }
+        } else {
+            geolocation.lat = latitude;
+            geolocation.lng = longitude;
+        }
+
+        // Tác vụ xử hình ảnh lên storage
+        async function storeImage(image) {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage();
+                const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+                const storageRef = ref(storage, filename);
+                const uploadTask = uploadBytesResumable(storageRef, image);
+                uploadTask.on('state_changed',
+                    (snapshot) => {
+                        // Observe state change events such as progress, pause, and resume
+                        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        // eslint-disable-next-line default-case
+                        switch (snapshot.state) {
+                            case 'paused':
+                                console.log('Upload is paused');
+                                break;
+                            case 'running':
+                                console.log('Upload is running');
+                                break;
+                        }
+                    },
+                    (error) => {
+                        reject(error);
+                    },
+                    () => {
+                        // Handle successful uploads on complete
+                        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                            resolve(downloadURL);
+                        });
+                    }
+                );
+            });
+        }
+
+        // Tác vụ chờ các Promise trả về từ hàm storeImage đã được giải quyết (resolve) hoặc bị từ chối (reject)
+        const imgUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+        ).catch((error) => {
+            setLoading(false)
+            toast.error("Images not uploaded");
+            return;
+        });
+
+        // Tạo fromData để lưu trữ lên firestore
+        const formDataCopy = {
+            ...formData,
+            imgUrls,
+            geolocation,
+            timestamp: serverTimestamp(),
+        };
+        delete formDataCopy.images;
+        !formDataCopy.offer && delete formDataCopy.discountedPrice;
+        delete formDataCopy.latitude;
+        delete formDataCopy.longitude;
+        const docRef = await addDoc(collection(db, "listings"), formDataCopy);
+        setLoading(false);
+        toast.success("Listing created");
+        navigate(`/category/${formDataCopy.type}/${docRef.id}`);
+    }
+
+    if (loading) {
+        return <Spinner />;
     }
 
     return (
         <main className='max-w-xl px-2 mx-auto'>
             <h1 className='text-3xl text-center mt-6 font-bold'>Create A Listing</h1>
-            <form className='bg-sky-100 p-8 mt-6'>
+            <form onSubmit={onSubmit} className='bg-sky-100 p-8 mt-6'>
                 {/* Sell Or Rent */}
                 <p className='text-lg font-semibold'>Sell / Rent</p>
                 <div className='flex'>
@@ -42,7 +199,7 @@ export default function CreateListing() {
                         ${type === "sale" ? "bg-white text-black" : "bg-slate-600 text-white"}`}
                         type='button'
                         id='type'
-                        value="sale">
+                        value="rent">
                         Rent
                     </button>
                 </div>
@@ -136,6 +293,34 @@ export default function CreateListing() {
                     placeholder='Address'
                     value={address}
                     required />
+                {!geolocationEnabled && (
+                    <div className='flex space-x-6 justify-start mb-6'>
+                        <div>
+                            <p className='text-lg font-semibold'>Latitude</p>
+                            <input
+                                onChange={onChange}
+                                className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center'
+                                type='number'
+                                min="-90"
+                                max="90"
+                                id='latitude'
+                                value={latitude}
+                                required />
+                        </div>
+                        <div>
+                            <p className='text-lg font-semibold'>Longitude</p>
+                            <input
+                                onChange={onChange}
+                                className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center'
+                                type='number'
+                                id='longitude'
+                                min="-180"
+                                max="180"
+                                value={longitude}
+                                required />
+                        </div>
+                    </div>
+                )}
 
                 {/* Description */}
                 <p className='text-lg font-semibold'>Description</p>
